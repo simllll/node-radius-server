c9ddfb6b70ade6da409a522fb20bf1be21487a6b
Akif Rabbani <akif@rabbani.my>
2021-01-24 05:24:26 +0800

    Added filter by status code

--- src/auth/HTTPAuth.ts
+++ src/auth/HTTPAuth.ts
@@ -13,24 +13,32 @@ export class HTTPAuth implements IAuthentication {
 	}
 
 	async authenticate(username: string, password: string) {
-
 		let success = false;
 		try {
-			
 			await axios
-			  .post(this.url, {
-			    username: username,
-			    password: password
-			  })
-			  .then(res => {
-				    console.log("Code: " + res.status)
-				    if (res.status == 200) {
-				    	success = true;
-				    	console.log("Return code 200, authentication successful.");
-				    }
-			  });
+				.post(
+					this.url,
+					{
+						username,
+						password,
+					},
+					{
+						validateStatus(status) {
+							return status >= 200 && status < 500;
+						},
+					}
+				)
+				.then((res) => {
+					console.log(`Code: ${res.status}`);
+					if (res.status === 200) {
+						success = true;
+						console.log('Return code 200, HTTP authentication successful');
+					} else {
+						console.log('HTTP authentication failed');
+					}
+				});
 		} catch (err) {
-			console.error('HTTP auth failed');
+			console.error('HTTP response error');
 		}
 		return success;
 	}
